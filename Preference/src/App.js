f23c8c8c328702082ff8f75adb9683a42c9071ce
Doohyun Im <doohyun2040@gmail.com>
2021-09-28 19:17:14 +0900

    Preference and LoginLayout reset

--- Preference/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Navigation from './navigations';
-
-export default function App () {
-  return (
-    <Navigation/>
-  )
-}
--- Preference/src/components/index.js
+++
@@ -1,6 +0,0 @@
-import PrefButton from "./PrefButton";
-import Button from "./Button";
-import Indicator from "./Indicator";
-import BackButton from "./BackButton";
-
-export {PrefButton, Button, Indicator, BackButton};
